7484e73b50deeda3fb5904f91e7f5b635b0f8de3
JamilJalal <jamil.jalal94@gmail.com>
2018-09-17 12:16:20 -0400

    Added Collection View and Tab BarA

---
+++ Flix/Flix/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Jamil Jalal on 9/17/18.
+//  Copyright © 2018 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
